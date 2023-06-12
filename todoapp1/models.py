da3f485f4ffc6b67518d5fe74ca041801e8c2ff9
Daniel Assani <devasddaniel@gmail.com>
2023-06-12 09:38:44 +0200

    all

--- todoapp1/models.py
+++ todoapp1/models.py
@@ -1,3 +1,19 @@
+from unicodedata import category
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Categorie(models.Model):
+    title = models.CharField(max_length=255)
+    def __str__(self):
+        return f'{self.title}'
+
+class Todo(models.Model):
+    
+    title = models.CharField(max_length=255)
+    content = models.fields.TextField()
+    category = models.ForeignKey(Categorie, null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return f'{self.title}'
+
+
